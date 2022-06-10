101939d79f7db259d3ba8f59de36a95f55114ebd
unknown <ajiriosiobe@gmail.com>
2022-06-10 13:03:23 +0100

    responsive adjustment

--- artic-app/src/Components/Footer.js
+++ artic-app/src/Components/Footer.js
@@ -21,34 +21,37 @@ const Footer = () => {
             </div>
 
             <div className="footer-first-div2 text-center col-lg-6 pt-4">
-                <div id="div1" className="justify-content-center">
-                    <h4>COMPANY</h4>
-                    <div className="pt-2">
-                        <p>About</p>
-                        <p> Careers</p>
-                        <p>Mobile</p>
+                <div className="row">
+                    <div id="div1" className="col-lg-4 col-sm-4">
+                        <h4>COMPANY</h4>
+                        <div className="pt-2">
+                            <p>About</p>
+                            <p> Careers</p>
+                            <p>Mobile</p>
+                        </div>
+                        
                     </div>
+
+                    <div id="div2" className="col-lg-4 col-sm-4">
+                        <h4>CONTACT</h4>
+                        <div className="pt-2">
+                            <p>FAQ</p>
+                            <p>Press</p>
+                            <p>Affiliates</p>
+                        </div>
                     
-                </div>
-
-                <div id="div2" className="justify-content-center">
-                    <h4>CONTACT</h4>
-                    <div className="pt-2">
-                        <p>FAQ</p>
-                        <p>Press</p>
-                        <p>Affiliates</p>
                     </div>
-                   
-                </div>
 
-                <div id="div3" className="justify-content-center">
-                    <h4>MORE</h4>
-                    <div className="pt-2">
-                        <p>Airlines</p>
-                        <p> Airfees</p>
-                        <p>Lowfare Tips</p>
-                    </div> 
+                    <div id="div3" className="col-lg-4 col-sm-4">
+                        <h4>MORE</h4>
+                        <div className="pt-2">
+                            <p>Airlines</p>
+                            <p> Airfees</p>
+                            <p>Lowfare Tips</p>
+                        </div> 
+                    </div>
                 </div>
+                
             </div>
         </div>
       </section>
